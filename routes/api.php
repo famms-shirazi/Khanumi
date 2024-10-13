d91a31f3ee463e67bf1853bdc738f80012aa58ee
y.rezaei <yaserdinani1999@gmail.com>
2024-10-13 20:27:41 +0330

    feat: add UserController

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::prefix('v1')->group(function () {
+    Route::resource('users', App\Http\Controllers\API\V1\UserController::class);
+});
